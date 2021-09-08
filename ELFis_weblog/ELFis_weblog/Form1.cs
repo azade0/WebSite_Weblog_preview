22c72027e1be864bd53ea13ecddf80561fa1af63
azade0 <90243656+azade0@users.noreply.github.com>
2021-09-08 17:28:51 +0430

    Add files via upload

---
+++ ELFis_weblog/ELFis_weblog/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ELFis_weblog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.Aquamarine;
+            TransparencyKey = Color.Aquamarine;
+
+            var pos1 = this.PointToScreen(label1.Location);
+            pos1 = pictureBox1.PointToClient(pos1);
+            label1.Parent = pictureBox1;
+            label1.Location = pos1;
+            label1.BackColor = Color.Transparent;
+
+            var pos2 = this.PointToScreen(label2.Location);
+            pos2 = pictureBox1.PointToClient(pos2);
+            label2.Parent = pictureBox1;
+            label2.Location = pos2;
+            label2.BackColor = Color.Transparent;
+
+            var pos3 = this.PointToScreen(label3.Location);
+            pos3 = pictureBox1.PointToClient(pos3);
+            label3.Parent = pictureBox1;
+            label3.Location = pos3;
+            label3.BackColor = Color.Transparent;
+
+            /*webBrowser1.AutoScroll = true;
+            webBrowser1.HorizontalScroll.Enabled = false;
+            webBrowser1.HorizontalScroll.Visible = false;
+            */
+            //webBrowser1.Dock = DockStyle.Fill;
+
+            webBrowser1.Navigate(new Uri(@"http://www.elfis.blogfa.com"));
+            
+        }
+
+       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //webBrowser1.Document.Body.SetAttribute("With", "100%");
+            
+        }
+    }
+}
